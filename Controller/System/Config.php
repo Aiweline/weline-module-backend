4f77ff9339b50b454dfad5783f12212f1b8d7325
Aiweline <32635766+Aiweline@users.noreply.github.com>
2024-06-16 20:29:46 +0800

    fix

--- Controller/System/Config.php
+++ Controller/System/Config.php
@@ -11,20 +11,43 @@ declare(strict_types=1);
 
 namespace Weline\Backend\Controller\System;
 
+use Weline\CacheManager\Console\Cache\Clear;
 use Weline\Framework\Manager\ObjectManager;
 
 class Config extends \Weline\Framework\App\Controller\BackendController
 {
-    public function set($key, $value, $type = 'json')
+    public function set()
     {
+        $key = $this->request->getGet('key');
+        $value = $this->request->getGet('value');
+        $type = $this->request->getGet('type','');
         /**@var \Weline\Backend\Model\Config $config */
         $config = ObjectManager::getInstance(\Weline\Backend\Model\Config::class);
         $config->setConfig($key, $value, 'Weline_Backend');
-        $fetchName = 'fetch' . ucfirst($type);
+        $fetchName = 'fetch' . ucfirst((string)$type);
+        if(!method_exists($this, $fetchName)){
+            $this->getMessageManager()->addWarning(__('保存失败，请重试!不支持的类型：%1', $type));
+            return $this->fetch();
+        }
+        # 清理缓存
+        /**@var Clear $cache */
+        $cache = ObjectManager::getInstance(Clear::class);
+        $cache->execute(['-f']);
+        ob_clean();
         try {
-            return $this->$fetchName($this->success());
+            if($type === 'json'){
+                return $this->$fetchName($this->success('保存成功!'));
+            }else{
+                $this->getMessageManager()->addSuccess(__('保存成功,缓存清理成功！'));
+                $this->redirect($this->request->getReferer());
+            }
         } catch (\Exception $exception) {
-            return $this->$fetchName($this->exception($exception));
+            if($type === 'json'){
+                return $this->$fetchName($this->error(__('保存失败! %1', $exception->getMessage())));
+            }else{
+                $this->getMessageManager()->addWarning(__('保存失败! %1', $exception->getMessage()));
+                $this->redirect($this->request->getReferer());
+            }
         }
     }
 }
